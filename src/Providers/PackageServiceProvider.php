38281891b88545727da6552c26496f36fd44beeb
hnineiphyu-ken <hnineiphyucu@gmail.com>
2024-08-15 15:15:15 +0630

    Update PackageServiceProvider.php

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -2,20 +2,21 @@
 
 namespace KenNebula\AYAPaymentIntegration\Providers;
 
+use Illuminate\Foundation\AliasLoader;
 use Illuminate\Support\ServiceProvider;
 
 class PackageServiceProvider extends ServiceProvider
 {
     public function register() : void 
     {
-      $this->app->bind('AYA', function($app) {
-          return new AYA();  // Create an instance of AYA
-      });
-      $this->app->alias('AYA', \KenNebula\AYAPaymentIntegration\AYA::class);
+      // $this->app->bind('AYA', function($app) {
+      //     return new AYA();  // Create an instance of AYA
+      // });
     }
 
     public function boot() : void 
     {
+        AliasLoader::getInstance()->alias('AYA', \KenNebula\AYAPaymentIntegration\AYA::class);
         if ($this->app->runningInConsole()) {
             // Example: Publishing configuration file
             $this->publishes([
