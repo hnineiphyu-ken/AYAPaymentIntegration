e11137eba9e25ff03dac24adf864bcebbabd790f
hnineiphyu-ken <hnineiphyucu@gmail.com>
2024-08-15 14:29:43 +0630

    Update PackageServiceProvider.php

--- src/Providers/PackageServiceProvider.php
+++ src/Providers/PackageServiceProvider.php
@@ -8,16 +8,15 @@ class PackageServiceProvider extends ServiceProvider
 {
     public function register() : void 
     {
-        
     }
 
     public function boot() : void 
     {
         if ($this->app->runningInConsole()) {
-
+            // Example: Publishing configuration file
             $this->publishes([
-              __DIR__.'/config/config.php' => config_path('aya.php'),
-            ], 'config');
+              __DIR__.'/../config/config.php' => config_path('aya.php'),
+          ], 'config');
         
           }
     }
